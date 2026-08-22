a92f608d6f8d4d1ef325689e6682af0fe056986e
track2000one <track2000one@gmail.com>
2026-08-22 12:34:04 +0300

    Expose users API endpoint

--- backend/health/urls.py
+++ backend/health/urls.py
@@ -1,6 +1,7 @@
 from rest_framework.routers import DefaultRouter
-from .views import AuditLogViewSet, ClinicVisitViewSet, CommitteeReferralViewSet, EmployeeViewSet, HealthCenterViewSet, InjuryCaseViewSet, LabTestViewSet, VaccinationViewSet
+from .views import AuditLogViewSet, ClinicVisitViewSet, CommitteeReferralViewSet, EmployeeViewSet, HealthCenterViewSet, InjuryCaseViewSet, LabTestViewSet, UserViewSet, VaccinationViewSet
 router=DefaultRouter()
+router.register('users',UserViewSet,basename='users')
 router.register('health-centers',HealthCenterViewSet)
 router.register('employees',EmployeeViewSet)
 router.register('lab-tests',LabTestViewSet)
